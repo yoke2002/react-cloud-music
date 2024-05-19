87e06f7b0b745d2b401e7960e6ca8c1bf10393c0
yoke2002 <weironglu9586@gmail.com>
2024-05-19 18:16:38 +0800

    feat: recommend list

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from '../../components/Slider'
+import RecommendList from '../../components/List'
 
 function Recommend() {
   //mock 数据
@@ -10,9 +11,20 @@ function Recommend() {
     }
   })
 
+  const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+    return {
+      id: 1,
+      picUrl:
+        'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
+      playCount: 17171122,
+      name: '朴树、许巍、李健、郑钧、老狼、赵雷'
+    }
+  })
+
   return (
     <div>
       <Slider bannerList={bannerList} />
+      <RecommendList recommendList={recommendList} />
     </div>
   )
 }
