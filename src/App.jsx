51aab1009f197a0b8e308133b96c7d4dd6d61708
yoke2002 <weironglu9586@gmail.com>
2024-05-19 14:06:08 +0800

    fix: global init style invalid

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { RouterProvider } from 'react-router-dom'
+
+import router from './router'
+
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from './style'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 
-import router from './router'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 )
