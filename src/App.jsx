079cc199aac4ac35a74f85919519939a58595eb1
yoke2002 <weironglu9586@gmail.com>
2024-05-19 16:34:48 +0800

    chore: integrate redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import router from './router'
+import store from './store'
 
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from './style'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
       <RouterProvider router={router} />
-    </>
+    </Provider>
   )
 }
 
